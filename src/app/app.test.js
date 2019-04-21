0cb76d66db0f40d88fa262e79e7a2dcbb7d15592
Romain Dorgueil <romain@dorgueil.net>
2019-04-21 12:11:33 +0200

    Stupid fix for tests, provide all the context required before attempting to render. This may not be the right way to do it, but at least tests passes and app renders.

--- src/app/app.test.js
+++ src/app/app.test.js
@@ -1,9 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { MemoryRouter } from "react-router-dom";
+import createStore from "../store";
+import { Provider } from "react-redux";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
+
+  const { store } = createStore();
+  ReactDOM.render(
+    <Provider store={store}>
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    </Provider>,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
